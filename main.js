ed5fe47fcca5aec76b46dfd84c50172322fc71e1
Alexandra Liggins <alexandraliggins@Alexandras-MacBook-Air.local>
2018-07-08 19:27:30 -0400

    scratch projects

---
+++ main.js
@@ -0,0 +1,14 @@
+(function () {
+  $(document).ready(init)
+
+  function init(){
+    $('#modal1').modal();
+    $('#modal2').modal();
+    $('#modal3').modal();
+    $('#modal4').modal();
+    $('#modal5').modal();
+    $('#modal6').modal();
+    $('#modal7').modal();
+  }
+
+})();
